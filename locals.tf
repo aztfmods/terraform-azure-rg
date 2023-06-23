dd99e9a6c3f447a780e1596de3e3895c09a5f749
dkool <dennis.kool@cloudnation.nl>
2023-06-23 20:33:21 +0200

    cleanup

--- locals.tf
+++ locals.tf
@@ -1,8 +1,7 @@
-locals {
-  all_groups = length(var.groups) == 0 ? {
-    default = {
-      region = try(var.region, "westeurope")
-    }
-  } : var.groups
-}
-
+#locals {
+#  all_groups = length(var.groups) == 0 ? {
+#    default = {
+#      region = try(var.region, "westeurope")
+#    }
+#  } : var.groups
+#}
--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
+# single
 resource "azurerm_resource_group" "rg" {
-  for_each = local.all_groups
+  for_each = length(var.groups) == 0 && var.region != null ? { default = var.region } : {}
 
-  name     = "rg-${var.workload}-${var.environment}${each.key != "default" ? "-${each.key}" : ""}${var.suffix != "" ? "-${var.suffix}" : ""}"
+  name     = "rg-${var.workload}-${var.environment}${var.suffix != "" ? "-${var.suffix}" : ""}"
+  location = each.value
+}
+
+# multiple
+resource "azurerm_resource_group" "rgs" {
+  for_each = var.groups
+
+  name     = "rg-${var.workload}-${var.environment}-${each.key}${var.suffix != "" ? "-${var.suffix}" : ""}"
   location = each.value.region
 }
 
